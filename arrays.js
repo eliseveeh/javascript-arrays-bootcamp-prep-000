5d1a4b461b1b9574df76a68916911dbbccbc25c8
Elizabeth Veeh <elise.veeh@gmail.com>
2018-01-15 22:40:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,7 +9,3 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element);
   return array;
 }
-
-function addElementToEndOfArray(array, element){
-  return [array..., element]
-}

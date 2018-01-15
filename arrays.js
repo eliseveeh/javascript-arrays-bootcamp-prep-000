e4f82768ad9f5ef8c103c96a410d4ab977ccb1fc
Elizabeth Veeh <elise.veeh@gmail.com>
2018-01-15 22:38:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,37 +1,4 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 
-function addElementToBeginningOfArray(array, element) {
-    return [element, ...array]
-}
 
-function destructivelyAddElementToBeginningOfArray(array, element){
-  array.unshift(element);
-  return array;
-}
-
-
-function addElementToEndOfArray(array, element){
-  return [array..., element]
-}
-function destructivelyAddElementToEndOfArray(array, element){
-  return array.push(element);
-}
-function accessElementInArray(array, index){
-  return array[index];
-}
-function destructivelyRemoveElementFromBeginningOfArray(array, index){
-  array.shift();
-  return array;
-}
-function removeElementFromBeginningOfArray(array, index){
-  var x = array.slice(0, 1)
-  return array;
-}
-function destructivelyRemoveElementFromEndOfArray(array){
-  array.pop();
-  return array;
-}
-function removeElementFromEndOfArray(array){
-  array.slice(-1);
-}
